e4c8196725b61cbe1ae24c9b79b6ee5061766b58
queen-cece <ccezeonu3@gmail.com>
2018-07-24 14:41:07 +0000

    automatically backed up by learn

--- mothers_day.rb
+++ mothers_day.rb
@@ -1,5 +1,5 @@
 ## Define your method, mothers_day, below. Go through the README and update your method as needed!
 def mothers_day (your_name)
-puts "mothers_day(Cece)"
+puts "mothers_day (Cece)"
 end 
-say_happy (Mothers Day, Mom)
+mothers_day ("Cece")
